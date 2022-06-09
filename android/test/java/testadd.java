b4dea681d5b696cf9445ad94c94c90482d1f0863
ryanbourdais <rbourd4@lsu.edu>
2022-06-09 16:04:17 -0500

    changed testadd

--- android/test/java/testadd.java
+++ android/test/java/testadd.java
@@ -1,8 +1,9 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Test
+class JUnit5ExampleAddTest {
 public void testAdd() {
     assertEquals(42, Integer.sum(19, 23));
     System.out.println("This test method should be run as well");
+}
 }
