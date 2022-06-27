adfc3598632b974450fe49eb9e6fa4185e3d34d5
ryanbourdais <rbourd4@lsu.edu>
2022-06-27 16:02:16 -0500

    added android api 30

--- __tests__/Google-Play-test.js
+++ __tests__/Google-Play-test.js
@@ -4,9 +4,5 @@ PlayServices.sync();
 
 test('Google Play Status', async () => {
   currentStatus = await PlayServices.checkPlayServicesStatus();
-  // .then(response => response.json())
-  // .then(data => {
-  //   return data;
-  // });
   expect(currentStatus).toBe(PlayServices.GooglePlayServicesStatus.AVAILABLE);
 });
