fcc66fb3ca403a2bbcd9a0f7f3e6fb75a45a65c3
ryanbourdais <rbourd4@lsu.edu>
2022-07-01 12:44:56 -0500

    fix

--- __tests__/Google-Play-test.js
+++
@@ -1,8 +0,0 @@
-import PlayServices from 'react-native-play-services';
-
-PlayServices.sync();
-
-test('Google Play Status', async () => {
-  currentStatus = await PlayServices.checkPlayServicesStatus();
-  expect(currentStatus).toBe(PlayServices.GooglePlayServicesStatus.AVAILABLE);
-});
