d7dc32fbbbc7e62daffa51278e4ddc1fd177ee2d
ryanbourdais <rbourd4@lsu.edu>
2022-07-06 12:56:24 -0500

    fix

--- android/app/src/test/java/testadd.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class JUnit5ExampleAddTest {
-public void testAdd() {
-    assertEquals(42, Integer.sum(19, 23));
-    System.out.println("This test method should be run as well");
-}
-}
